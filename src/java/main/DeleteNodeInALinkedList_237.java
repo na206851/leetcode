31193b415b8cd2c0e31e6791dbe4f73d8cdf8032
Nikita Shavrov <na206851@gmail.com>
2024-08-31 17:30:28 +0800

    add new task

---
+++ src/java/main/DeleteNodeInALinkedList_237.java
@@ -0,0 +1,15 @@
+public class DeleteNodeInALinkedList_237 {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
