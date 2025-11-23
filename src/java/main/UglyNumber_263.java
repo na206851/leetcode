aa3657311d98e4b8f92be1afaadcd17dc996a9d1
Nikita Shavrov <na206851@gmail.com>
2025-11-23 19:28:46 +1200

    add first task in a long time

---
+++ src/java/main/UglyNumber_263.java
@@ -0,0 +1,20 @@
+public class UglyNumber_263 {
+
+    public boolean isUgly(int n) {
+        if (n < 0) {
+            return false;
+        }
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else if (n % 5 == 0) {
+                n /= 5;
+            } else if (n % 3 == 0) {
+                n /= 3;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
