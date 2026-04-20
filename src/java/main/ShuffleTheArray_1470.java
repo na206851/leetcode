3b35bbc08cff9fa85efe659935a249727cf6f739
Nikita Shavrov <na206851@gmail.com>
2026-04-20 21:25:06 +1200

    fast code

--- src/java/main/ShuffleTheArray_1470.java
+++ src/java/main/ShuffleTheArray_1470.java
@@ -3,15 +3,12 @@ import java.util.Arrays;
 public class ShuffleTheArray_1470 {
     public static int[] shuffle(int[] nums, int n) {
         int[] result = new int[nums.length];
-        int[] left = Arrays.copyOfRange(nums, 0, n);
-        int[] right = Arrays.copyOfRange(nums, n, nums.length);
         int leftX = 0;
-        int rightY = 0;
         for (int i = 0; i < nums.length; i++) {
             if (i % 2 == 0) {
-                result[i] = left[leftX++];
+                result[i] = nums[leftX++];
             } else {
-                result[i] = right[rightY++];
+                result[i] = nums[n++];
             }
         }
         return result;
